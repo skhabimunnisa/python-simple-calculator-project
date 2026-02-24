e899df6f13f2a3507d37a75d864ad7720a8baa3d
skhabimunnisa <skhabimunnisha924@gmail.com>
2026-02-24 17:03:07 +0530

    Fix variable naming and improve calculator logic

--- Simple_Caluculator.py
+++ Simple_Caluculator.py
@@ -1,24 +1,25 @@
 print("Simple Caluculator")
-Number1=float("enter first number:")
-Number2=float("enter second number:")
-print("The first Number is",Number1)
-print("The second Number is",Number2)
-choice =input("Enter your choice(+,-,*,/,%):")
+number1=float(input("enter first number:"))
+number2=float(input("enter second number:"))
+print("The first Number is",number1)
+print("The second Number is",number2)
+choice = input("Enter your choice(+,-,*,/,%):")
 if choice=='+':
-print("Addition of" ,Number1,"and",Number2,"is:",Number1+Number2)
+    print("Addition of" ,number1,"and",number2,"is:",number1+number2)
 elif choice=='-':
-print("Substration of" ,Number1,"and",Number2,"is:",Number1-Number2)
-elif choice=="*":
-print("Multiplication of" ,Number1,"and",Number2,"is:",Number1*Number2)
-elif choice=="/":
-if Number2!=0:
-    print("Divison of" ,Number1,"and",Number2,"is:",Number1/Number2)
+    print("Substration of" ,number1,"and",number2,"is:",number1-number2)
+elif choice=='*':
+    print("Multiplication of" ,number1,"and",number2,"is:",number1*number2)
+elif choice=='/':
+if number2!=0:
+    print("Divison of" ,number1,"and",number2,"is:",number1/number2)
     else:
     print("division my zero is error!")
 elif choice=="%":
-    print("Remainder of" ,Number1,"and",Number2,"is:",Number1%Number2)
+    print("Remainder of" ,number1,"and",number2,"is:",number1%number2)
 else:
-    print("enter invalid opertion,try again!")
+    print(" invalid opertion,try again!")
+
 
 
 
